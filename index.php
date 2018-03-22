4a33976143e988caf10d2a57f181cd80909e7002
Pablo Bruno DW <hola@pablobrunodw.com>
2018-03-22 20:19:36 -0300

    Prueba de modificacion de archivo

--- index.php
+++ index.php
@@ -12,7 +12,6 @@
 
 		<!-- Hojas de Estilo -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		
 		<link rel="stylesheet" type="text/css" href="/res/css/icons/icon.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
